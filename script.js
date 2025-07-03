f09ef48b0897705ce589e8522d5e52f430fe27dc
Anonimku <mochamad.prasetiooo@gmail.com>
2025-07-03 12:24:25 +0700

    script.js

--- script.js
+++ script.js
@@ -1,45 +1,63 @@
-const map = L.map('map').setView([-6.9, 107.6], 7); // ubah ke lokasi Indonesia
+const map = L.map('map').setView([-6.9, 107.6], 7);
 
+// Peta dasar
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Muat data stasiun dan jalur
-Promise.all([
-  fetch('data/stasiun.json').then(res => res.json()),
-  fetch('data/jalur.json').then(res => res.json())
-]).then(([stasiunData, jalurData]) => {
-  // Tampilkan stasiun
-  stasiunData.forEach(stasiun => {
-    L.circleMarker([stasiun.lat, stasiun.lon], { radius: 6, color: 'blue' })
-      .addTo(map)
-      .bindPopup(`<b>${stasiun.nama}</b>`);
+// Load stasiun.geojson
+fetch('stasiun.json.geojson')
+  .then(res => res.json())
+  .then(data => {
+    L.geoJSON(data, {
+      pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+          radius: 6,
+          color: 'blue',
+          fillColor: 'lightblue',
+          fillOpacity: 0.9
+        }).bindPopup(`<b>${feature.properties.nama}</b>`);
+      }
+    }).addTo(map);
   });
 
-  // Tampilkan jalur
-  jalurData.forEach(jalur => {
-    const latlngs = jalur.koordinat.map(k => [k.lat, k.lon]);
-    L.polyline(latlngs, { color: 'black' }).addTo(map);
-  });
+// Load jalur.geojson dan animasikan kereta
+fetch('jalur.json.geojson')
+  .then(res => res.json())
+  .then(data => {
+    const allCoords = [];
 
-  // 🚆 Animasikan kereta
-  animateTrain(jalurData[0].koordinat); // contoh animasi di jalur pertama
-});
+    L.geoJSON(data, {
+      style: {
+        color: 'black',
+        weight: 3
+      },
+      onEachFeature: function (feature) {
+        const coords = feature.geometry.coordinates.map(c => [c[1], c[0]]); // GeoJSON = [lon, lat]
+        allCoords.push(...coords);
+      }
+    }).addTo(map);
+
+    setTimeout(() => animateTrain(allCoords), 1000); // jalankan animasi setelah peta selesai
+  });
 
 function animateTrain(coords) {
   let i = 0;
-  const marker = L.circleMarker([coords[0].lat, coords[0].lon], {
+  const marker = L.circleMarker(coords[0], {
     radius: 8,
-    color: 'red'
+    color: 'red',
+    fillColor: 'pink',
+    fillOpacity: 0.9
   }).addTo(map);
 
   function move() {
     if (i < coords.length - 1) {
-      marker.setLatLng([coords[i].lat, coords[i].lon]);
+      marker.setLatLng(coords[i]);
       i++;
-      setTimeout(move, 500); // jeda 500ms per langkah
+      setTimeout(move, 300); // jeda antar titik
     }
   }
 
   move();
 }
+
